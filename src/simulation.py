d48cc2bd8dc5709b9e0c762cd4e8835a222c7c7b
marjanalbouye <marjan.albouye73@gmail.com>
2023-02-02 12:36:31 -0700

    Clean up code.

--- src/simulation.py
+++ src/simulation.py
@@ -1,13 +1,8 @@
-from itertools import combinations_with_replacement as combo
 import operator
-import os
 
-import gsd.hoomd
 import hoomd
 import hoomd.md
 from mbuild.formats.hoomd_forcefield import create_hoomd_forcefield
-import numpy as np
-import parmed as pmd
 
 
 class Simulation:
@@ -45,19 +40,20 @@ class Simulation:
     -------
 
     """
+
     def __init__(
-        self,
-        system,
-        r_cut=2.5,
-        dt=0.0003,
-        auto_scale=True,
-        seed=42,
-        restart=None,
-        pppm_kwargs={"Nx": 16, "Ny": 16, "Nz": 16},
-        gsd_write_freq=1e4,
-        gsd_file_name="trajectory.gsd",
-        log_write_freq=1e3,
-        log_file_name="sim_data.txt"
+            self,
+            system,
+            r_cut=2.5,
+            dt=0.0003,
+            auto_scale=True,
+            seed=42,
+            restart=None,
+            pppm_kwargs={"Nx": 16, "Ny": 16, "Nz": 16},
+            gsd_write_freq=1e4,
+            gsd_file_name="trajectory.gsd",
+            log_write_freq=1e3,
+            log_file_name="sim_data.txt"
     ):
         self.system = system
         self.r_cut = r_cut
@@ -69,8 +65,8 @@ class Simulation:
         self.log_file_name = log_file_name
         self.ref_mass = max([atom.mass for atom in self.system.atoms])
         pair_coeffs = list(set(
-            (atom.type, atom.epsilon, atom.sigma)for atom in self.system.atoms
-                        )
+            (atom.type, atom.epsilon, atom.sigma) for atom in self.system.atoms
+        )
         )
         self.ref_energy = max(pair_coeffs, key=operator.itemgetter(1))[1]
         self.ref_distance = max(pair_coeffs, key=operator.itemgetter(2))[2]
@@ -90,10 +86,10 @@ class Simulation:
         self.integrator = None
         # Create forcefield objects and initial snapshot from self.system
         self.init_snap, self.forcefield, refs = create_hoomd_forcefield(
-                structure=self.system,
-                r_cut=self.r_cut,
-                auto_scale=self.auto_scale,
-                pppm_kwargs=self.pppm_kwargs 
+            structure=self.system,
+            r_cut=self.r_cut,
+            auto_scale=self.auto_scale,
+            pppm_kwargs=self.pppm_kwargs
         )
         if self.restart:
             self.sim.create_state_from_gsd(self.restart)
@@ -113,7 +109,7 @@ class Simulation:
     @property
     def dt(self):
         return self._dt
-    
+
     @dt.setter
     def dt(self, value):
         self._dt = value
@@ -126,9 +122,9 @@ class Simulation:
             return self.sim.operations.integrator.methods[0]
         else:
             raise RuntimeError(
-                    "No integrator, or method has been set yet. "
-                    "These will be set once one of the run functions "
-                    "have been called for the first time."
+                "No integrator, or method has been set yet. "
+                "These will be set once one of the run functions "
+                "have been called for the first time."
             )
 
     def set_integrator_method(self, integrator_method, method_kwargs):
@@ -145,13 +141,13 @@ class Simulation:
             A diction of parameter:value for the integrator method used
 
         """
-        if not self.integrator: # Integrator and method not yet created
+        if not self.integrator:  # Integrator and method not yet created
             self.integrator = hoomd.md.Integrator(dt=self.dt)
             self.integrator.forces = self.forcefield
             self.sim.operations.add(self.integrator)
-            new_method = integrator_method(**method_kwargs) 
+            new_method = integrator_method(**method_kwargs)
             self.sim.operations.integrator.methods = [new_method]
-        else: # Replace the existing integrator method
+        else:  # Replace the existing integrator method
             self.integrator.methods.remove(self.method)
             new_method = integrator_method(**method_kwargs)
             self.integrator.methods.append(new_method)
@@ -191,29 +187,29 @@ class Simulation:
         # Set up box resizer
         resize_trigger = hoomd.trigger.Periodic(period)
         box_ramp = hoomd.variant.Ramp(
-                A=0, B=1, t_start=self.sim.timestep, t_ramp=int(n_steps)
+            A=0, B=1, t_start=self.sim.timestep, t_ramp=int(n_steps)
         )
         initial_box = self.sim.state.box
         final_box = hoomd.Box(
-                Lx=final_box_lengths[0],
-                Ly=final_box_lengths[1],
-                Lz=final_box_lengths[2]
+            Lx=final_box_lengths[0],
+            Ly=final_box_lengths[1],
+            Lz=final_box_lengths[2]
         )
         box_resizer = hoomd.update.BoxResize(
-                box1=initial_box,
-                box2=final_box,
-                variant=box_ramp,
-                trigger=resize_trigger
+            box1=initial_box,
+            box2=final_box,
+            variant=box_ramp,
+            trigger=resize_trigger
         )
         self.sim.operations.updaters.append(box_resizer)
         self.set_integrator_method(
-                integrator_method=hoomd.md.methods.NVT,
-                method_kwargs={"tau": tau_kt, "filter": self.all, "kT": kT},
+            integrator_method=hoomd.md.methods.NVT,
+            method_kwargs={"tau": tau_kt, "filter": self.all, "kT": kT},
         )
         if thermalize_particles:
             self._thermalize_system(kT)
         self.sim.run(n_steps)
-    
+
     def run_langevin(
             self,
             n_steps,
@@ -226,15 +222,15 @@ class Simulation:
     ):
         """"""
         self.set_integrator_method(
-                integrator_method=hoomd.md.methods.Langevin,
-                method_kwargs={
-                        "filter": self.all,
-                        "kT": kT,
-                        "alpha": alpha,
-                        "tally_reservoir_energy": tally_resivoir_energy,
-                        "default_gamma": default_gamma,
-                        "default_gamma_r": default_gamma_r,
-                    }
+            integrator_method=hoomd.md.methods.Langevin,
+            method_kwargs={
+                "filter": self.all,
+                "kT": kT,
+                "alpha": alpha,
+                "tally_reservoir_energy": tally_resivoir_energy,
+                "default_gamma": default_gamma,
+                "default_gamma_r": default_gamma_r,
+            }
         )
         if thermalize_particles:
             self._thermalize_system(kT)
@@ -255,19 +251,19 @@ class Simulation:
     ):
         """"""
         self.set_integrator_method(
-                integrator_method=hoomd.md.methods.NPT,
-                method_kwargs={
-                    "kT": kT,
-                    "S": pressure,
-                    "tau": tau_kt,
-                    "tauS": tau_pressure,
-                    "couple": couple,
-                    "box_dof": box_dof,
-                    "rescale_all": rescale_all,
-                    "gamma": gamma,
-                    "filter": self.all,
-                    "kT": kT
-                }
+            integrator_method=hoomd.md.methods.NPT,
+            method_kwargs={
+                "kT": kT,
+                "S": pressure,
+                "tau": tau_kt,
+                "tauS": tau_pressure,
+                "couple": couple,
+                "box_dof": box_dof,
+                "rescale_all": rescale_all,
+                "gamma": gamma,
+                "filter": self.all,
+                "kT": kT
+            }
         )
         if thermalize_particles:
             self._thermalize_system(kT)
@@ -276,8 +272,8 @@ class Simulation:
     def run_NVT(self, n_steps, kT, tau_kt, thermalize_particles=True):
         """"""
         self.set_integrator_method(
-                integrator_method=hoomd.md.methods.NVT,
-                method_kwargs={"tau": tau_kt, "filter": self.all, "kT": kT},
+            integrator_method=hoomd.md.methods.NVT,
+            method_kwargs={"tau": tau_kt, "filter": self.all, "kT": kT},
         )
         if thermalize_particles:
             self._thermalize_system(kT)
@@ -286,23 +282,23 @@ class Simulation:
     def run_NVE(self, n_steps):
         """"""
         self.set_integrator_method(
-                integrator_method=hoomd.md.methods.NVE,
-                method_kwargs={"filter": self.all}
+            integrator_method=hoomd.md.methods.NVE,
+            method_kwargs={"filter": self.all}
         )
         self.sim.run(n_steps)
 
     def temperature_ramp(self, n_steps, kT_start, kT_final):
         return hoomd.variant.Ramp(
-                A=kT_start,
-                B=kT_final,
-                t_start=self.sim.timestep,
-                t_ramp=int(n_steps)
+            A=kT_start,
+            B=kT_final,
+            t_start=self.sim.timestep,
+            t_ramp=int(n_steps)
         )
 
     def _thermalize_system(self, kT):
         if isinstance(kT, hoomd.variant.Ramp):
             self.sim.state.thermalize_particle_momenta(
-                    filter=self.all, kT=kT.range[0]
+                filter=self.all, kT=kT.range[0]
             )
         else:
             self.sim.state.thermalize_particle_momenta(filter=self.all, kT=kT)
@@ -310,10 +306,10 @@ class Simulation:
     def _add_hoomd_writers(self):
         """Creates gsd and log writers"""
         gsd_writer = hoomd.write.GSD(
-                filename=self.gsd_file_name,
-                trigger=hoomd.trigger.Periodic(int(self.gsd_write_freq)),
-                mode="wb",
-                dynamic=["momentum"]
+            filename=self.gsd_file_name,
+            trigger=hoomd.trigger.Periodic(int(self.gsd_write_freq)),
+            mode="wb",
+            dynamic=["momentum"]
         )
 
         logger = hoomd.logging.Logger(categories=["scalar", "string"])
